091fa1c84cecd5e5061b585142aa7152d0d0329b
Kurtis Nicks <87857751+hatedpw@users.noreply.github.com>
2021-09-03 14:07:53 +0930

    Update ft_atoi.c

--- libft/libc-funcs/ft_atoi.c
+++ libft/libc-funcs/ft_atoi.c
@@ -1,14 +1,6 @@
 #include "libft.h"
 /* I hate atoi, this function caused me so many issues during piscine. 
 * take a string of char that are meant to be num convert into int. 
-* create 3 variables, ret holds result, value is to turn neg.
-* idx is counter for array. 
-* intial check for spacing at beginning of str. 
-* check if neg or positive. 
-* if neg make value = -1 to get result. 
-* then convert char to int if digit. 
-* keep looping til find char that is not number. 
-* return the ret * value. added (int) to verify that returns as an int. 
 */
 int	ft_atoi(const char *str)
 {
